thick location  border fixed, d1 dist  fixed, missing factor on features fixed

IrisFeatureExtraction.py
@@ -32,13 +32,13 @@ def create_filter(dx,dy,crop_amount):
 
     # Filter 1 parameters
     f = 1/dy
-    sig_x = dx
-    sig_y = dy
+    # sig_x = dx
+    # sig_y = dy
 
     # x, y = np.mgrid[-7:8, -7:8]
     x, y = np.mgrid[0:crop_amount, 0:512]
 
-    filter = np.exp(-(x**2 / (2*sig_x**2) + y**2 / (2*sig_y**2))) * np.cos(2 * np.pi * f * (x**2 + y**2)**0.5)
+    filter = (1/(2*np.pi*dx*dy))*np.exp(-(x**2 / (2*dx**2) + y**2 / (2*dy**2))) * np.cos(2 * np.pi * f * (x**2 + y**2)**0.5)
     
     return filter
 

IrisMatching.py
@@ -27,7 +27,7 @@ def calculate_L1_distance(feature_vec_train, feature_vec_test):
     d1= [] 
 
     for i in feature_vec_test:
-        d1.append(np.argmin(np.abs(np.sum(feature_vec_train - i, axis =1))))
+        d1.append(np.argmin(np.sum(np.abs(feature_vec_train - i), axis =1)))
 
     return d1
 