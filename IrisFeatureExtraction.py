62605093eb25e47be1f1585ee233074cfef4a168
Eric <ericaragundi@gmail.com>
2023-11-04 19:59:12 -0400

    features changes  fixed

--- IrisFeatureExtraction.py
+++ IrisFeatureExtraction.py
@@ -35,8 +35,8 @@ def create_filter(dx,dy,crop_amount):
     # sig_x = dx
     # sig_y = dy
 
-    # x, y = np.mgrid[-7:8, -7:8]
-    x, y = np.mgrid[0:crop_amount, 0:512]
+    x, y = np.mgrid[-7:8, -7:8]
+    # x, y = np.mgrid[0:crop_amount, 0:512]
 
     filter = (1/(2*np.pi*dx*dy))*np.exp(-(x**2 / (2*dx**2) + y**2 / (2*dy**2))) * np.cos(2 * np.pi * f * (x**2 + y**2)**0.5)
     
--- IrisMatching.py
+++ IrisMatching.py
@@ -27,7 +27,7 @@ def calculate_L1_distance(feature_vec_train, feature_vec_test):
     d1= [] 
 
     for i in feature_vec_test:
-        d1.append(np.argmin(np.sum(np.abs(feature_vec_train - i), axis =1)))
+        d1.append(np.argmin(np.sum(np.abs(i - feature_vec_train), axis =1)))
 
     return d1
 
@@ -40,7 +40,7 @@ def calculate_L2_distance(feature_vec_train, feature_vec_test):
     d2= [] 
 
     for i in feature_vec_test:
-        d2.append(np.argmin(np.sum(np.square(feature_vec_train - i), axis =1)))
+        d2.append(np.argmin(np.sum(np.square(i - feature_vec_train), axis =1)))
         #print(d2)
 
     return d2
@@ -53,18 +53,33 @@ def calculate_cosine_distance(feature_vec_train, feature_vec_test):
     #index of min distance for each test vector
     d3= [] 
     
-    cosine_distance_array = []
-    for i in feature_vec_test:
-        A = np.sqrt(np.sum(np.square(i)))
-        for m,n in enumerate(feature_vec_train):
-            B = np.sqrt(np.sum(np.square(n)))
-            numerator = np.sum(np.multiply(i, n))
-            cosine = np.divide(numerator, (A*B))
-            cosine_distance_array.append(cosine)
-        d3.append(np.argmin(cosine_distance_array))
+    
+    for idx_test,test_vec in enumerate(feature_vec_test):
+        #simran code
+        # A = np.sqrt(np.sum(np.square(i)))
+        # for m,n in enumerate(feature_vec_train):
+        #     B = np.sqrt(np.sum(np.square(n)))
+        #     numerator = np.sum(np.multiply(i, n))
+        #     cosine = np.divide(numerator, (A*B))
+        #     cosine_distance_array.append(cosine)
+        # d3.append(np.argmin(cosine_distance_array))
 
         #print(d3)
+        cosine_distance_array = []
 
+        for idx,train_vector in enumerate(feature_vec_train):
+            # print("working on index: ",idx)
+            numerator = np.dot(test_vec.T,train_vector)
+            # print("numerator at index : ",idx ," is : ",numerator)
+            denominator = np.dot(np.linalg.norm(test_vec),np.linalg.norm(train_vector))
+            # print("denominator at index : ",idx ," is : ",denominator)
+
+            d3_unit = 1 - (numerator/denominator)
+            # print("d3_unit at index : ",idx ," is : ",d3_unit)
+            cosine_distance_array.append(d3_unit)
+
+        # print("arg min at index test : ",idx_test ," is : ",np.argmin(cosine_distance_array))
+        d3.append(np.argmin(cosine_distance_array))
     return d3
 
 def max_class(index_array):
